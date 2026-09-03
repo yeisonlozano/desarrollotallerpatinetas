764cc452deb1cd4b5759e9a5fb543c5b8b49869c
Lozano-Arenas Yeison-Fabian <yeison.lozano@inetum.com>
2026-09-03 11:39:29 -0500

    Inicio modulo ordenes frontend

---
+++ app/ordenes/nuevo/page.tsx
@@ -0,0 +1,9 @@
+export default function NuevaOrdenPage() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold">
+        Nueva Orden de Servicio
+      </h1>
+    </div>
+  );
+}
--- app/ordenes/page.tsx
+++ app/ordenes/page.tsx
@@ -1,9 +1,28 @@
+import Link from "next/link";
+
+/*
+ * Página principal de órdenes.
+ */
 export default function OrdenesPage() {
   return (
     <div>
-      <h1 className="text-3xl font-bold">
+
+      <h1 className="text-3xl font-bold mb-6">
         Órdenes de Servicio
       </h1>
+
+      <Link
+        href="/ordenes/nuevo"
+        className="
+          bg-green-600
+          text-white
+          px-4
+          py-2
+       "
+      >
+        Nuevo Orden
+      </Link>
+
     </div>
   );
 }
